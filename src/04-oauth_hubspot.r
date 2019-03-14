3a16de42e76b870e64dfc4744551a25e6765219e
troopertino <TMerl@PARK-SIEBEN.INTRANET>
2019-03-14 18:31:58 +0100

    Formatted the Function and first use of the API

--- src/04-oauth_hubspot.r
+++ src/04-oauth_hubspot.r
@@ -1,32 +1,80 @@
 library(httr)
 
-authUrl = "https://app.hubspot.com/oauth/authorize"
-tokenUrl = "https://api.hubapi.com/oauth/v1/token"
+urls = list(
+    authUrl = "https://app.hubspot.com/oauth/authorize",
+    tokenUrl = "https://api.hubapi.com/oauth/v1/token",
+    baseUrl = "https://api.hubapi.com",
 
-analyticsBase = "https://api.hubapi.com/analytics/v2/reports"
-templatesBase = "https://api.hubapi.com/content/api/v2/templates"
+    getCalls = list(
+        #analytics = "analytics/v2/reports",
+        templates = "content/api/v2/templates",
+        contacts = "contacts/v1/lists/all/contacts/all",
+        emailCampaigns = "email/public/v1/campaigns/by-id",
+        blog = "content/api/v2/blogs",
+        workflows = "automation/v3/workflows",
+        forms = "forms/v2/forms",
+        landingpages = "content/api/v2/pages"
+    )
+)
 
-clientId = "6861bfc7-f088-4720-a689-e49030fe8e44"
-clientSecret = "d603bb1e-1435-4ece-b11c-6be56ca31ad4"
-
-app = oauth_app("hubspot",clientId, clientSecret)
-
-hubspot = oauth_endpoint(authorize = authUrl, access = tokenUrl)
-
-token = oauth2.0_token(hubspot,app, scope = c("content","business-intelligence"))
-
-tkn = token[["credentials"]][["access_token"]]
-
-breakdown = "sessions"
-timeperiod = "daily"
-start = "20190101"
-end = "20190131"
-
-breakTimeUrl = paste(analyticsBase,breakdown,timeperiod, sep = "/")
-reqUrl = paste0(breakTimeUrl,"?start=",start,"&end=",end)
-
-resp = GET(templatesBase, add_headers(Accept = "application/json", Authorization = paste("Bearer", tkn, sep = " "), `Content-Type` = "application/json"))
-data = jsonlite::fromJSON(rawToChar(resp$content))
+hubAuth = function() {
+    clientId = "6861bfc7-f088-4720-a689-e49030fe8e44"
+    clientSecret = "d603bb1e-1435-4ece-b11c-6be56ca31ad4"
+    app = oauth_app("hubspot",clientId, clientSecret)
+    hubspot = oauth_endpoint(authorize = urls$authUrl, access = urls$tokenUrl)
+    token = oauth2.0_token(hubspot,app, scope = c("contacts", "content", "automation", "business-intelligence", "forms"))
+    tkn = token[["credentials"]][["access_token"]]
+    
+    tkn
+}
 
 
-## Finally enough rights
+hubCalls = function(tkn) {
+    urlEndings = names(urls$getCalls)
+
+    answer = list()
+    for (u in urlEndings) {
+        print(u)
+        url = paste(urls$baseUrl, urls$getCalls[[u]], sep = "/")
+        resp = try(GET(url, add_headers(Accept = "application/json", Authorization = paste("Bearer", tkn, sep = " "), `Content-Type` = "application/json")))
+        print(resp)
+        #data = try(jsonlite::fromJSON(rawToChar(resp$content)))
+        if ("try-error" %in% class(data) ) {
+            next
+        } else {
+            answer[[u]] = resp
+        }
+    }
+    answer
+}
+
+respToList = function(respList) {
+    listNames = names(respList)
+    newList = list()
+    for (l in listNames) {
+        temp = jsonlite::fromJSON(rawToChar(respList[[l]]$content))
+        newList[[l]] = temp
+    }
+
+    newList
+}
+
+tkn = hubAuth()
+respList = hubCalls(tkn)
+respList = respToList(respList)
+
+
+contactList = respList$contacts
+
+contactDf = respList$contacts$contacts
+
+row.names(contactDf) = contactDf$properties$firstname
+
+row.names(contactDf) = paste(contactDf$properties$firstname[,1], contactDf$properties$lastname[,1], contactDf$vid, sep = "_")
+
+
+as.POSIXlt(contactDf$addedAt, origin = "1970-01-01")
+
+View(contactDf$`identity-profiles`)
+
+View(as.data.frame(respList$contacts$contacts$`form-submissions`))
