977cf5979334781f5c8129d01bfaa7b28af64202
troopertino <35485536+troopertino@users.noreply.github.com>
2019-03-14 08:39:06 +0100

    Creating an index Page and deleting it afterwads

---
+++ src/browserpage.py
@@ -0,0 +1,12 @@
+import os
+
+def createPage():
+    htmlFile='index.html'
+    f=open(htmlFile, 'w')
+    f.write("<html><body><p>Python has created this Page for the API Call. It can now be closed.</p></body></html>")
+    f.close()
+    return htmlFile
+
+value = createPage()
+os.remove(value)
+
