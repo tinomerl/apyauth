443c0097725ab20c04172e0fd7aafba1a29444d3
troopertino <35485536+troopertino@users.noreply.github.com>
2019-03-15 17:20:10 +0100

    added readme.md

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# Functional Class for OAuth2.0
+
+## Current Version
+0.1
+
+## Works with
+* Hubspot Auth
--- src/tests.py
+++ src/tests.py
@@ -1,7 +1,9 @@
 import sys
 sys.path.insert(0,'./src/lib')
 import oauth2class
+import hubApi
 
+import ast
 
 clientid = '6861bfc7-f088-4720-a689-e49030fe8e44'
 clientsecret = 'd603bb1e-1435-4ece-b11c-6be56ca31ad4'
@@ -10,13 +12,25 @@ scope = ['contacts', 'content', 'automation', 'business-intelligence', 'forms']
 instanz.authEndpoints('https://app.hubspot.com/oauth/authorize',' https://api.hubapi.com/oauth/v1/token')
 instanz.authUrlBuild(scope)
 tkn = instanz.getToken()
-tkn
 
-import requests
-import ast
+hub = hubApi.HubApi(tkn)
+data = hub.analytics('breakdown', start = "20190101", end = "20190102", breakdown= 'sources', timePeriod= 'daily')
 
-headers = {'Authorization':  'Bearer ' + tkn, 'Content-Type' : 'application/json'}
 
-res = requests.get('https://api.hubapi.com/analytics/v2/reports/sources/total?start=20190101&end=20190101', headers = headers)
+len(data)
+import pandas as pd
+import json
 
-ast.literal_eval(res.content)
+data.keys()
+
+
+data[list(data)[0]][1]
+
+df = pd.DataFrame()
+
+
+json.dumps(data['breakdowns'])
+
+pd.read_json(json.dumps(data['breakdowns']), orient = "records")
+
+data
