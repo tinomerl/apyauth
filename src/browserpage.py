75694eacc4e4ee2bcf2b6089996e375737fb796b
troopertino <35485536+troopertino@users.noreply.github.com>
2019-03-15 13:22:37 +0100

    deleting of test files

--- src/browserpage.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def createPage():
-    htmlFile='index.html'
-    f=open(htmlFile, 'w')
-    f.write("<html><body><p>Python has created this Page for the API Call. It can now be closed.</p></body></html>")
-    f.close()
-    return htmlFile
