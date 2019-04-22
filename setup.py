ada88214beb7cc0a2fba3a7691bc6aac9fc1fbc0
troopertino <35485536+troopertino@users.noreply.github.com>
2019-04-22 17:57:52 +0200

    changed setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 setuptools.setup(name = 'oauth2py', 
-    version = '0.3',
+    version = '0.3.1',
     packages = setuptools.find_packages(),
     description = 'Handy module for oauth2.0 Authentication.',
     install_requires =[
